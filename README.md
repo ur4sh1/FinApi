1303f3eb2156de9c9120e183c386979442afee7a
Lucas <lucas.edgerly@gmail.com>
2022-11-30 19:35:27 -0400

    Create Deposit/statement/debit

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ Exercício de Node.js
 ## :books: Funcionalidades
 
 [x] Deve ser possível criar umma conta
-[] Deve ser possível buscar o extrato bancário do cliente
-[] Deve ser possível realizar um depósito
+[x] Deve ser possível buscar o extrato bancário do cliente
+[x] Deve ser possível realizar um depósito
 [] Deve ser possível realizar um saque
 [] Deve ser possível buscar o extrato bancário do cliente por data
 [] Deve ser possível atualizar dados da conta do cliente
@@ -18,8 +18,8 @@ Exercício de Node.js
 ## :books: Regras de negócio
 
 [x] Não deve ser possível cadastrar uma conta com CPF já existente
-[] Não deve ser possível fazer depósito em uma conta não existente
-[] Não deve ser possível buscar extrato em uma conta não existente
+[x] Não deve ser possível buscar extrato em uma conta não existente
+[x] Não deve ser possível fazer depósito em uma conta não existente
 [] Não deve ser possível fazer saque em uma conta não existente
 [] Não deve ser possível excluir uma conta não existente
 [] Não deve ser possível fazer saque quando o saldo for insuficiente
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { response } = require("express");
+const { response, request } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid")
 
@@ -7,20 +7,43 @@ app.use(express.json());
 
 const customers = [];
 
-// cpf - String
-// name - String
-// id - uuid
-// statement []
+//Middleware
+function verifyIfExistsAccountCPF( request, response, next) {
 
+  const { cpf } =  request.headers;
+  const customer =  customers.find(customer => customer.cpf === cpf);
+
+  if(!customer){
+    return response.status(400).json({error: "Customer not found!"})
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation)=>{
+    if(operation.type === "credit"){
+      return acc + operation.amount;
+    }else{
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
 
 app.post("/account", (request, response)=>{
   const {cpf, name} = request.body;
   const customerAlreadyExists = customers.some((customer)=> customer.cpf === cpf);
 
+  //função que verifica se já existe um cliente cadastrado
   if(customerAlreadyExists) {
     return response.status(400).json({error:"Customer already exists!"})
   }
 
+  //insere um cliente no array "customer"
   customers.push({
     cpf,
     name,
@@ -29,7 +52,50 @@ app.post("/account", (request, response)=>{
   })
 
   return response.status(201).send();
+});
 
+//app.use(verifyIfExistsAccountCPF);
+
+app.get("/statement",verifyIfExistsAccountCPF, (request, response)=>{
+  const { customer } = request;
+  return response.json(customer.statement);
+});
+
+app.post("/deposit",verifyIfExistsAccountCPF, (request, response)=>{
+  const { description, amount } = request.body;
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credt"
+  }
+  
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send({msg: "sucessfuly!"});
+});
+
+app.post("/withdraw",verifyIfExistsAccountCPF, (request, response)=>{
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount){
+    return response.status(400).send({error: "Insufficient funds!"})
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit"
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 })
 
 app.listen(3330);
